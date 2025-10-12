052f88ec9420e3bde25f213692ef993db432c24a
Valentina Piccione <81562115+SwellPoem@users.noreply.github.com>
2025-10-12 12:21:23 +0200

    Update assets.js

--- script/utils/assets.js
+++ script/utils/assets.js
@@ -2,26 +2,26 @@ export default [
     {
         type: "gltf",
         name: "room",
-        path: "../my_scene.glb"
+        path: "./my_scene.glb"
     },
     {
         type: "videoTexture",
         name: "screen-animation",
-        path: "../assets/video/schermo3.mp4"
+        path: "./assets/video/schermo3.mp4"
     },
     {
         type: "audio",
         name: "typing",
-        path: "../assets/audio/keyboard.mp3"
+        path: "./assets/audio/keyboard.mp3"
     },
     {
         type: "audio",
         name: "sax",
-        path: "../assets/audio/sax.mp3"
+        path: "./assets/audio/sax.mp3"
     },
     {
         type: "audio",
         name: "radio",
-        path: "../assets/audio/broken-radio-frequency.mp3"
+        path: "./assets/audio/broken-radio-frequency.mp3"
     },
 ];
